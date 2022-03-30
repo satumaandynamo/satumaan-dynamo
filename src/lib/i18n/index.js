13cef995697be55e0242547fdb6c62977de99aed
Saaga Helin <saaga.helin@gmail.com>
2022-03-30 17:00:09 +0300

    adds language detector

--- src/lib/i18n/index.js
+++ src/lib/i18n/index.js
@@ -1,26 +1,23 @@
 import i18next from 'i18next'
-import detector from 'i18next-browser-languagedetector'
-// import AsyncStoragePlugin from 'i18next-react-native-async-storage'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 import en from './en'
 import fi from './fi'
 
-i18next
-    .use(detector)
-    // .use(AsyncStoragePlugin('fi'))
-    .init({
-        interpolation: {
-            escapeValue: false,
-        },
-        debug: true,
-        resources: {
-            fi,
-            en,
-        },
-        react: {
-            wait: true,
-        },
-    })
+i18next.use(LanguageDetector).init({
+  interpolation: {
+    escapeValue: false,
+  },
+  fallbackLng: 'fi',
+  debug: true,
+  resources: {
+    fi,
+    en,
+  },
+  react: {
+    wait: true,
+  },
+})
 
 const i18n = i18next
 export default i18n
