28e67d4f445be37f8fc38e136a8a523b83f72394
Saaga Helin <saaga.helin@gmail.com>
2022-04-01 20:26:36 +0300

    removes unused old comps/views

--- src/components/app/loading.js
+++
@@ -1,9 +0,0 @@
-import { CircularProgress } from "@material-ui/core";
-
-function Loading() {
-  <CircularProgress>
-    return <span className="visually-hidden">Loading...</span>;
-  </CircularProgress>;
-}
-
-export default Loading;
